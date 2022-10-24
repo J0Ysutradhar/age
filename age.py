77555395ea0d8e1cf44a7867ff0f7c2fc45fa405
Joy3301 <91220470+Joy3301@users.noreply.github.com>
2022-10-24 12:11:04 +0600

    Age adult
    Etx

---
+++ age.py
@@ -0,0 +1,10 @@
+print("what is your name")
+name = str(input())
+print("what is your age")
+age = int(input())
+if age<18:
+	print(name +" you are kid")
+elif age>=18:
+	print(name  +" you are adult")
+
+	
